602d0b88da0782143e3e798a033ac605bd84e20d
admin <nailfax1991@yandex.ru>
2023-06-07 14:40:42 +0200

    update3

--- gui.py
+++ gui.py
@@ -12,30 +12,36 @@ def find_color():
     color_code = find_color_code(color_name)
     
     if color_code is not None:
+        
         saved_colors.append((color_name, color_code))
         update_saved_colors()
-        combo.set('')  # Сбросить выбор цвета после добавления в список сохраненных
+        
 
 def find_color_code(color_name):
     workbook = openpyxl.load_workbook('C:/Users/admin/Desktop/Палитра кодов.xlsx')
     worksheet = workbook.active
     
     for row in worksheet.iter_rows(values_only=True):
-        if row[0] == color_name:
+        if row[0] == color_name or f'{row[0]}\n' == color_name:
+            
+            print("saved_colors: ", saved_colors)
+            print('row[0]: ', row[0])
             new_format = []
             for _ in list(row[1:4]):
                 new_format.append(round(_))
-                print(new_format)
             return rgb_to_hex(tuple(new_format))  # Возвращаем код цвета
 
     return None  # Если цвет не найден
 
 def update_saved_colors():
+    
     colors_listbox.delete(0, tk.END)
     for color in saved_colors:
         colors_listbox.insert(tk.END, fr'{color[0]}: {color[1]}')
+    
+    
 def rgb_to_hex(rgb):
-    #return '%02x%02x%02x' % rgb
+    
     hex_value = "{:02X}{:02X}{:02X}".format(rgb[0], rgb[1], rgb[2])
     return hex_value
 def save_palette():
@@ -63,6 +69,10 @@ def save_palette():
     
     print(f'Палитра успешно сохранена в файл: {output_file}')
     os.system(f"swatch generate  -i {output_file} -o {file_path}")
+def delete_selected_item():
+    selected_index = colors_listbox.curselection()
+    if selected_index:
+        colors_listbox.delete(selected_index)
 # Инициализация окна
 root = tk.Tk()
 root.title('Поиск и сохранение цветов')
@@ -81,16 +91,20 @@ combo.grid(row=0, column=1, padx=10)
 find_button = tk.Button(frame, text='Найти', command=find_color)
 find_button.grid(row=0, column=2, padx=10)
 
+delete_button = tk.Button(frame, text="Удалить", command=delete_selected_item)
+delete_button.grid(row=1, column=2, padx=10)
+
 colors_listbox = tk.Listbox(root, width=40)
-colors_listbox.pack(pady=10)
+colors_listbox.pack(padx=10)
 
 save_frame = tk.Frame(root)
 save_frame.pack()
 
 
 
-save_button = tk.Button(root, height=25, text='Сохранить палитру', command=save_palette)
+save_button = tk.Button(root, text='Сохранить палитру', command=save_palette)
 save_button.pack(pady=10)
+
 
 
 
